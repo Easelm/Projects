ec335738b097466cf48d27babcd30cda0f1e9b36
Easelm <easelm@live.com>
2013-01-27 13:22:19 -0500

    ADDING Support for ArkCore

---
+++ ArkCore/Dire Maul Arena Challenge/sqls/creature/UPDATES/10-1-2012 Update.sql
@@ -0,0 +1,7 @@
+UPDATE creature_template SET type='1' WHERE (entry='88022');
+UPDATE creature_template SET type='7' WHERE (entry='88023');
+UPDATE creature_template SET type='7' WHERE (entry='80700');
+UPDATE creature_template SET type='7' WHERE (entry='88500');
+UPDATE creature_template SET unit_class='1' WHERE (entry='88002');
+UPDATE creature_template SET unit_class='1' WHERE (entry='88500');
+UPDATE `creature_template` SET `ScriptName`='npc_dm_wave_portals' WHERE (`entry`='88501');
